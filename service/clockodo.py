39975843e9e7f59f420b29cf4df34aca9629c30f
Christian Dähn <christian.daehn@gmail.com>
2021-02-03 13:10:35 +0100

    using customer/project/service mapping

--- service/clockodo.py
+++ service/clockodo.py
@@ -2,40 +2,102 @@ import calendar
 import logging
 from configparser import ConfigParser
 from datetime import datetime
+from functools import lru_cache
 from logging import getLogger, basicConfig
 
 import requests
 from requests.auth import HTTPBasicAuth
 
-from entity.day import DayEntry, WorkshopDayEntry, WorkshopPrepDayEntry, InternalDayEntry
+from entity.day import DayEntry, WorkshopDayEntry, WorkshopPrepDayEntry, InternalDayEntry, CustomerDayEntry, \
+    ShorttimeDayEntry
 from service.gcal import GoogleCalendarService, GoogleCalendarServiceBuilder
 
 
-class ClockodoDay(object):
-    def __init__(self, start_date: datetime, end_date: datetime, comment, customer_id, service_id, billable):
-        self.comment = comment
+class ClockodoIdMapping(object):
+    def __init__(self, customer_id: int, project_id: int, service_id: int, billable: int = 1):
         self.billable = billable
         self.service_id = service_id
+        self.project_id = project_id
         self.customer_id = customer_id
+
+
+class ClockodoDay(object):
+    def __init__(self, start_date: datetime, end_date: datetime, comment: str, mapping: ClockodoIdMapping):
+        self.comment = comment
+        self.billable = mapping.billable
+        self.service_id = mapping.service_id
+        self.customer_id = mapping.customer_id
+        self.project_id = mapping.project_id
         self.end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S')
         self.start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
 
 
+class ClockodoService(object):
+    base_url = 'https://my.clockodo.com/api'
+
+    def __init__(self, email: str, api_key: str):
+        self.email = email
+        self.api_key = api_key
+
+    def _get_auth(self) -> HTTPBasicAuth:
+        return HTTPBasicAuth(self.email, self.api_key)
+
+    def _get_user_id(self) -> str:
+        users = requests.get(self.base_url + '/users', auth=self._get_auth()).json()['users']
+        user_id = list(filter(lambda x: x['email'] == self.email, users))[0]['id']
+        return user_id
+
+
+class ClockodoResolutionService(ClockodoService):
+    def __init__(self, email: str, api_key: str):
+        super().__init__(email, api_key)
+
+    def resolve_for(self, customer_name: str, project_name: str, service_name: str) -> ClockodoIdMapping:
+        resolved_customer = next(
+            filter(lambda customer: customer['name'] == customer_name, self._retrieve('customers')),
+            None)
+        customer_id = resolved_customer['id']
+        project_id = next(filter(lambda project: project['name'] == project_name, resolved_customer['projects']),
+                          None)['id']
+        service_id = next(filter(lambda service: service['name'] == service_name, self._retrieve('services')),
+                          None)['id']
+        return ClockodoIdMapping(customer_id, project_id, service_id)
+
+    @lru_cache(128)
+    def _retrieve(self, endpoint):
+        return requests.get(self.base_url + f'/{endpoint}', auth=self._get_auth()).json()[endpoint]
+
+
 class ClockodoDayMapper(object):
+
+    def __init__(self, resolution_service: ClockodoResolutionService):
+        self.resolution_service = resolution_service
+
     def to_clockodo_day(self, event: DayEntry):
         date = datetime.strptime(event.date, '%d.%m.%Y')
         start_time = datetime.strptime(event.start, '%H:%M')
         end_time = datetime.strptime(event.end, '%H:%M')
         start_date = datetime(date.year, date.month, date.day, start_time.hour, start_time.minute, 0)
         end_date = datetime(date.year, date.month, date.day, end_time.hour, end_time.minute, 0)
-        if event.__class__ == WorkshopDayEntry:
-            return ClockodoDay(start_date, end_date, event.comment, 1462125, 549396, 1)
-        elif event.__class__ == WorkshopPrepDayEntry:
-            return ClockodoDay(start_date, end_date, event.comment, 1462125, 572017, 1)
-        elif event.__class__ == InternalDayEntry or event.__class__ == DayEntry:
-            return ClockodoDay(start_date, end_date, event.comment, 1361511, 549394, 1)
+        mapping = None
+        if isinstance(event, WorkshopDayEntry):
+            mapping = self.resolution_service.resolve_for('Bayer AG',
+                                                          'Bayer AG - PM-Trainings 9-13 Bst.-Nr.: 2950100643',
+                                                          'Inhouse Schulung')
+        elif isinstance(event, WorkshopPrepDayEntry):
+            mapping = self.resolution_service.resolve_for('Bayer AG',
+                                                          'Bayer AG - PM-Trainings 9-13 Bst.-Nr.: 2950100643',
+                                                          'Inhouse Schulung Vor-/ Nachbereitung')
+        elif isinstance(event, InternalDayEntry):
+            mapping = self.resolution_service.resolve_for('it-agile GmbH', 'Vertrieb', 'Interne Arbeitszeit')
+        elif isinstance(event, CustomerDayEntry):
+            mapping = self.resolution_service.resolve_for('AOK Systems GmbH', 'Coach the Coaches', 'Coaching')
+        elif isinstance(event, ShorttimeDayEntry):
+            mapping = self.resolution_service.resolve_for('it-agile GmbH', 'Kurzarbeit', 'Interne Arbeitszeit')
         else:
             raise Exception("don't know how to map [%s]" % event)
+
+        return ClockodoDay(start_date, end_date, event.comment, mapping)
 
     def json_to_logging(self, json: dict):
         if 'services_name' in json:
@@ -45,14 +107,12 @@ class ClockodoDayMapper(object):
         return f"({service})[from={json['time_since']}, duration={json['duration_time']}, text={json['text']}]"
 
 
-class ClockodoEntryService(object):
-    base_url = 'https://my.clockodo.com/api'
+class ClockodoEntryService(ClockodoService):
 
     def __init__(self, email: str, api_key: str):
-        self.api_key = api_key
-        self.email = email
+        super().__init__(email, api_key)
         self.google_calendar_service = GoogleCalendarService(GoogleCalendarServiceBuilder())
-        self.day_entry_mapper = ClockodoDayMapper()
+        self.day_entry_mapper = ClockodoDayMapper(ClockodoResolutionService(email, api_key))
 
     def delete_entries(self, year: int, month: int):
         first_day = datetime(year, month, 1)
@@ -67,14 +127,6 @@ class ClockodoEntryService(object):
         last_day = datetime(year, month, calendar.monthrange(year, month)[1], 23, 59, 59)
         for event in self.google_calendar_service.events_in_range(first_day, last_day):
             self._enter(event)
-
-    def _get_auth(self) -> HTTPBasicAuth:
-        return HTTPBasicAuth(self.email, self.api_key)
-
-    def _get_user_id(self) -> str:
-        users = requests.get(self.base_url + '/users', auth=self._get_auth()).json()['users']
-        user_id = list(filter(lambda x: x['email'] == self.email, users))[0]['id']
-        return user_id
 
     def _current_entries(self, first_day, last_day):
         current_entries = requests.get(self.base_url + '/entries', auth=self._get_auth(),
@@ -92,12 +144,14 @@ class ClockodoEntryService(object):
         day_entry = self.day_entry_mapper.to_clockodo_day(event)
         response = requests.post(self.base_url + '/entries', auth=self._get_auth(),
                                  params={'customers_id': day_entry.customer_id,
+                                         'projects_id': day_entry.project_id,
                                          'billable': day_entry.billable,
                                          'services_id': day_entry.service_id,
                                          'time_since': day_entry.start_date_str,
                                          'time_until': day_entry.end_date_str,
                                          'text': day_entry.comment})
 
+        getLogger(self.__class__.__name__).debug(response.json())
         getLogger(self.__class__.__name__).info(
             f'inserting entry [{self.day_entry_mapper.json_to_logging(response.json()["entry"])}]...')
 
@@ -109,8 +163,8 @@ def main():
     email = config_parser['credentials']['email']
     api_key = config_parser['credentials']['api_key']
     entry_service = ClockodoEntryService(email, api_key)
-    entry_service.delete_entries(2021, 1)
-    entry_service.enter_events_from_gcal(2021, 1)
+    entry_service.delete_entries(2021, 2)
+    entry_service.enter_events_from_gcal(2021, 2)
 
 
 main()
